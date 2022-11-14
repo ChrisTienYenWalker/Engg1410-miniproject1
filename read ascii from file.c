b048dc168f5aaca03580a06361a020e50f05de03
nlevack <117744067+nlevack@users.noreply.github.com>
2022-11-14 16:03:26 -0500

    Add files via upload

---
+++ read ascii from file.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num;
+   FILE *fptr;
+
+   if ((fptr = fopen("C:\\asciiwritefile.txt","r")) == NULL){
+       printf("Error! opening file");
+
+       // Program exits if the file pointer returns NULL.
+       exit(1);
+   }
+    while(!feof(fptr)){
+        fscanf(fptr,"%d,", &num);
+        printf("%c", num);
+    }
+
+   fclose(fptr);
+
+   return 0;
+}
